4297f000f7493c28f6b50970ce8820e436afa026
ppyom <ouob77@kakao.com>
2024-09-04 16:43:04 +0900

    feat: Geolocation 사용 코드 구현

---
+++ src/apis/geolocation.js
@@ -0,0 +1,15 @@
+const handleSuccess = (position) => {
+  const { latitude, longitude } = position.coords;
+  return {
+    lat: latitude,
+    lon: longitude,
+  };
+};
+
+const getCurrentLocation = () => {
+  return new Promise((resolve, reject) =>
+    navigator.geolocation.getCurrentPosition(resolve, reject),
+  ).then(handleSuccess);
+};
+
+export { getCurrentLocation };
