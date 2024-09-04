3b6b1c3aff022ed9a59773a4b820019ea6eefac1
ppyom <ouob77@kakao.com>
2024-09-04 17:23:21 +0900

    feat: 다른 지역 날씨 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,39 @@ import WeatherAPI from './apis/weather.js';
 import WeatherBox from './components/WeatherBox/WeatherBox.jsx';
 import ButtonCon from './components/ButtonCon/ButtonCon.jsx';
 
+const cities = [
+  { id: 'current', value: '현재 위치' },
+  { id: 'seoul', value: '서울' },
+  { id: 'hongkong', value: '홍콩' },
+  { id: 'paris', value: '파리' },
+  { id: 'new york', value: '뉴욕' },
+];
+
 function App() {
   const [weatherInfo, setWeatherInfo] = useState(null);
+  const [city, setCity] = useState(null);
 
-  const getWeather = async ({ lat, lon }) => {
-    WeatherAPI.getCurrentWeather({ lat, lon }).then(setWeatherInfo);
+  const getWeather = async (options) => {
+    WeatherAPI.getCurrentWeather(options).then(setWeatherInfo);
+  };
+  const getCurrentWeather = () => {
+    getCurrentLocation() //
+      .then(getWeather)
+      .catch(console.error);
+  };
+  const handleCityBtnClick = (selected) => {
+    setCity(selected === 'current' ? null : selected);
   };
 
   useEffect(() => {
-    getCurrentLocation().then(getWeather).catch(console.error);
-  }, []);
+    !city ? getCurrentWeather() : getWeather({ q: city });
+  }, [city]);
 
   return (
     <main className="main">
       <h1>날씨 API 활용</h1>
       <WeatherBox data={weatherInfo || {}} />
-      <ButtonCon />
+      <ButtonCon cities={cities} onClick={handleCityBtnClick} />
     </main>
   );
 }
--- src/components/ButtonCon/ButtonCon.jsx
+++ src/components/ButtonCon/ButtonCon.jsx
@@ -1,13 +1,13 @@
 import Button from './Button/Button.jsx';
 
-const ButtonCon = () => {
+const ButtonCon = ({ cities, onClick }) => {
   return (
     <div>
-      <Button>현재 위치</Button>
-      <Button>서울</Button>
-      <Button>홍콩</Button>
-      <Button>파리</Button>
-      <Button>뉴욕</Button>
+      {cities.map((city) => (
+        <Button key={city.id} onClick={() => onClick(city.id)}>
+          {city.value}
+        </Button>
+      ))}
     </div>
   );
 };
