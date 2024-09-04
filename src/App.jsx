41c46553379696247fd9e344f2285072d0e5958d
ppyom <ouob77@kakao.com>
2024-09-04 16:16:33 +0900

    design: 기본 구조 구성

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
 function App() {
-  return <>App</>;
+  return (
+    <main className="main">
+      <h1>날씨 API 활용</h1>
+      <section>
+        <h2>지역 및 도시명</h2>
+        <p>24 &deg;C / 맑음</p>
+      </section>
+      <div>
+        <button>현재 위치</button>
+        <button>서울</button>
+        <button>홍콩</button>
+        <button>파리</button>
+        <button>뉴욕</button>
+      </div>
+    </main>
+  );
 }
 
 export default App;
