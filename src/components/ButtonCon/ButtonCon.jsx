97b94824a2fd347e8c0518053f4eb83d9db2e6fa
ppyom <ouob77@kakao.com>
2024-09-04 16:20:16 +0900

    design: 컴포넌트 분리 및 기본 스타일 제거

---
+++ src/components/ButtonCon/ButtonCon.jsx
@@ -0,0 +1,15 @@
+import Button from './Button/Button.jsx';
+
+const ButtonCon = () => {
+  return (
+    <div>
+      <Button>현재 위치</Button>
+      <Button>서울</Button>
+      <Button>홍콩</Button>
+      <Button>파리</Button>
+      <Button>뉴욕</Button>
+    </div>
+  );
+};
+
+export default ButtonCon;
---
+++ src/components/WeatherBox/WeatherBox.jsx
@@ -0,0 +1,10 @@
+const WeatherBox = () => {
+  return (
+    <section>
+      <h2>지역 및 도시명</h2>
+      <p>24 &deg;C / 맑음</p>
+    </section>
+  );
+};
+
+export default WeatherBox;
